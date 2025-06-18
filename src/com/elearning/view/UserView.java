236d29523fd31fdb069cbf89adfb1d291b32a082
Eve <92258125+Lilylac@users.noreply.github.com>
2025-06-18 22:42:19 +0700

    Fix Error "ls"

--- src/com/elearning/view/UserView.java
+++ src/com/elearning/view/UserView.java
@@ -5,7 +5,7 @@ public class UserView {
     public void show(){
         System.out.println("---------------------------USER DASHBOARD-------------------");
         System.out.println("-------------------------------------------------");
-        System.out.println("--- 1. Lihat Course    2. Edit Profile    3. Exit ");ls
+        System.out.println("--- 1. Lihat Course    2. Edit Profile    3. Exit ");
         System.out.println("-------------------------------------------------");
     }
 }
