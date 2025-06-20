f6b441ecff2a8a2785594ab3c0eeab658aded09e
Renzzy <pandjipangesturi@gmail.com>
2025-06-20 20:04:37 +0700

    Update UserView.java

--- src/com/elearning/view/UserView.java
+++ src/com/elearning/view/UserView.java
@@ -1,11 +1,27 @@
 package com.elearning.view;
 
-public class UserView {
+import com.elearning.services.CourseServiceImpl;
 
+import java.util.Scanner;
+
+public class UserView {
+    private CourseServiceImpl service;
+    Scanner scan = new Scanner(System.in);
+
+    public UserView(CourseServiceImpl service){
+        this.service = service;
+    }
     public void show(){
         System.out.println("---------------------------USER DASHBOARD-------------------");
         System.out.println("-------------------------------------------------");
-        System.out.println("--- 1. Lihat Course    2. Edit Profile    3. Exit ");
+        System.out.println("--- 1. Lihat Course    2. Course Saya    3. Edit Profile    3. Exit ");
         System.out.println("-------------------------------------------------");
+        System.out.print("Masukan pilihan: ");
+        int choose = Integer.valueOf(scan.nextLine());
+
+        switch (choose){
+            case 1 -> {
+                service.buyCourse();
+            }
     }
 }
