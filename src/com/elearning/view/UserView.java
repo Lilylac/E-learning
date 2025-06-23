3de14df361fdd61e278f1529717ae014fbcbf021
Renzzy <pandjipangesturi@gmail.com>
2025-06-23 16:35:56 +0700

    Update UserView.java

--- src/com/elearning/view/UserView.java
+++ src/com/elearning/view/UserView.java
@@ -18,45 +18,54 @@ public class UserView {
 
 
     public void show() {
-        System.out.println("=========================================================");
-        System.out.println("                     USER DASHBOARD                     ");
-        System.out.println("=========================================================");
-        System.out.println("  1. Beli Course   2. Course Saya   3. Edit Profile   99. Exit");
-        System.out.println("=========================================================");
-        System.out.print("Masukan pilihan: ");
-        int choose = Integer.valueOf(scan.nextLine());
-
-        switch (choose) {
-            case 1 -> {
-                System.out.println("=========================================================");
-                System.out.println("                     FORM BELI COURSE                    ");
-                System.out.println("=========================================================");
-                courseService.showAllCourse();
-                System.out.println("Masukan Id Anda : ");
-                int idUser = Integer.valueOf(scan.nextLine());
-                courseService.buyCourse(idUser);
-                show();
+        try {
+            System.out.println("=========================================================");
+            System.out.println("                     USER DASHBOARD                     ");
+            System.out.println("=========================================================");
+            System.out.println("  1. Beli Course   2. Course Saya   3. Edit Profile   4. Exit");
+            System.out.println("=========================================================");
+            System.out.print("Masukan pilihan: ");
+            int choose = Integer.valueOf(scan.nextLine());
+    
+            if(choose < 1 || choose > 4){
+                    System.out.println("Pilihan tidak ada");
+                    show();
             }
-            case 2 -> {
-                System.out.println("                                                                 ");
-                System.out.println("Masukan Id Anda : ");
-                int idUser = Integer.valueOf(scan.nextLine());
-                userService.showOwnedCourse(idUser);
-                show();
-
+    
+            switch (choose) {
+                case 1 -> {
+                    System.out.println("=========================================================");
+                    System.out.println("                     FORM BELI COURSE                    ");
+                    System.out.println("=========================================================");
+                    courseService.showAllCourse();
+                    System.out.println("Masukan Id Anda : ");
+                    int idUser = Integer.valueOf(scan.nextLine());
+                    courseService.buyCourse(idUser);
+                    show();
+                }
+                case 2 -> {
+                    System.out.println("                                                                 ");
+                    System.out.println("Masukan Id Anda : ");
+                    int idUser = Integer.valueOf(scan.nextLine());
+                    userService.showOwnedCourse(idUser);
+                    show();
+    
+                }
+                case 3 -> {
+                    System.out.println("                                                                 ");
+                    System.out.println("Masukan username: ");
+                    String username = String.valueOf(scan.nextLine());
+                    System.out.println("Masukan password: ");
+                    String password = String.valueOf(scan.nextLine());
+                    userService.editSelectedUser(username, password);
+                    show();
+                }
+                case 4 -> {
+                    return;
+                }
             }
-            case 3 -> {
-                System.out.println("                                                                 ");
-                System.out.println("Masukan username: ");
-                String username = String.valueOf(scan.nextLine());
-                System.out.println("Masukan password: ");
-                String password = String.valueOf(scan.nextLine());
-                userService.editSelectedUser(username, password);
-                show();
-            }
-            case 99 -> {
-                return;
-            }
+        } catch (NumberFormatException e){
+            System.out.println("Input tidak valid. Silahkan masukan angka");
         }
     }
 }
